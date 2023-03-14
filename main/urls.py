34a3fd2f6119a9252fd64e78ce95a736c3f09d1b
Evgeniy Ermakov <erma2001@mail.ru>
2023-03-14 19:05:47 +0700

    Добавлена ссылка на accounts

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,11 @@
 
-from django.urls import path
+from django.urls import path, include
 from .views import MainView, GalaryNsuemView, BaseOrderView, AuthorityOrderView
 
 
 urlpatterns = [
     path("", MainView.as_view(), name = "main"),
+    path("accounts/", include("users.urls")),
     path("nsuem_gal", GalaryNsuemView.as_view(), name = "nsuem_galary"),
     path("base_gal", BaseOrderView.as_view(), name = "base_galary"),
     path("authority_gal", AuthorityOrderView.as_view(), name = "authority_galary"),
