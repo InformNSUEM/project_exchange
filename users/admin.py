7fdf121a7fc4d7d2d9abcd02e9c22c54c9f81fbb
Evgeniy Ermakov <erma2001@mail.ru>
2023-03-14 19:07:31 +0700

    Добавлены и зарегистрирована модели, изменена модель юзера (добавлены доп поля, username стало не обязательным, email стало обязательным

---
+++ users/admin.py
@@ -0,0 +1,70 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm
+from .models import User, UserType, StudentUser, CustomerUser
+
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta:
+        model = UserCreationForm.Meta.model
+        fields = '__all__'
+        field_classes = UserCreationForm.Meta.field_classes
+
+class CustomUserAdmin(UserAdmin):
+
+    add_form = CustomUserCreationForm
+    add_fieldsets = (
+        
+        (None, {
+            'fields': ('email', 'password1', 'password2')
+        }),
+        ('Персональная информация', {
+            'fields': ('last_name', 'first_name', 'patronymic', 'dateBith')
+        }),
+        ('Служебная информация', {
+            'fields': ('userType',)
+        })
+    
+    )
+    fieldsets = (
+        (None, {
+            'fields': ('email', 'password')
+        }),
+        ('Персональная информация', {
+            'fields': ('last_name', 'first_name', 'patronymic', 'dateBith')
+        }),
+        ('Права', {
+            'fields': (
+                'is_active', 'is_staff', 'is_superuser',
+                'groups', 'user_permissions'
+                )
+        }),
+        ('Системные даты', {
+            'fields': ('last_login', 'date_joined')
+        }),
+        ('Служебная информация', {
+            'fields': ('userType',)
+        })
+
+    
+    )
+    list_display = ("email" ,"last_name","first_name", "patronymic" )
+
+admin.site.register(User, CustomUserAdmin)
+
+@admin.register(UserType)
+class UserTypeAdmin(admin.ModelAdmin):
+
+    list_display = ("name",)
+
+@admin.register(StudentUser)
+class StudentTypeAdmin(admin.ModelAdmin):
+
+    list_display = ("user",)
+
+@admin.register(CustomerUser)
+class CustomerTypeAdmin(admin.ModelAdmin):
+
+    list_display = ("user",)
+
+
