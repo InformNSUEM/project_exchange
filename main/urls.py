8523c323cb92ec2250bfa36dd9a9da4db5fb80db
Evgeniy <erma2001@mail.ru>
2023-12-04 17:27:47 +0700

    внесены изменения по демонстрации заявок

--- main/urls.py
+++ main/urls.py
@@ -6,9 +6,9 @@ from .views import MainView, GalaryNsuemView, BaseOrderView, AuthorityOrderView,
 urlpatterns = [
     path("", MainView.as_view(), name = "main"),
     path("accounts/", include("users.urls")),
-    path("nsuem_gal", GalaryNsuemView.as_view(), name = "nsuem_galary"),
-    path("base_gal", BaseOrderView.as_view(), name = "base_galary"),
-    path("authority_gal", AuthorityOrderView.as_view(), name = "authority_galary"),
+    #path("nsuem_gal", GalaryNsuemView.as_view(), name = "nsuem_galary"),
+    path("authority_galary", BaseOrderView.as_view(), name = "authority_galary"),
+    path("nsuem_galary", AuthorityOrderView.as_view(), name = "nsuem_galary"),
     path("business_application", Business_App.as_view(), name = "business_application"),
     path("system/", include("system.urls")),
         
